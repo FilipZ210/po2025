df4aa8794595c044358916133ed94e915868bcfa
Filip <fizajac@student.agh.edu.pl>
2025-10-06 15:48:09 +0200

    Zajecia 1: Hello i Choinka

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+  public static void main(String[] args) {
+    String star = "*";
+    for (int i = 0; i <= 5; i = i + 1) {
+    System.out.println(star);
+    star += "*";
+    }
+  }
+}
