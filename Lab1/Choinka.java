fdededdf087eaf0fd5a9090722f317fe859ba049
Filip <zajac@filip.com>
2025-10-08 14:40:48 +0200

    Choinka akutualizacja

--- Lab1/Choinka.java
+++ Lab1/Choinka.java
@@ -1,9 +1,10 @@
 public class Choinka {
-  public static void main(String[] args) {
-    String star = "*";
-    for (int i = 0; i <= 5; i = i + 1) {
-    System.out.println(star);
-    star += "*";
+    public static void main(String[] args) {
+        String star = "*";
+        int height = Integer.parseInt(args[0]);
+        for (int i = 0; i < height; i++) {
+            System.out.println(star);
+            star += "*";
+        }
     }
-  }
 }
