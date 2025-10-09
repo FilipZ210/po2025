fd861d70120cd6e48c4bfed864910a31bd628c2d
Filip <zajac@filip.com>
2025-10-09 10:44:54 +0200

    Lotto dokonczone

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,16 +1,14 @@
 import java.util.Random;
 import java.util.HashSet;
 
-public class Lotto {
-  public static void main(String[] args) {
-  Random rand = new Random();
-  HashSet<String> coupon = new HashSet<String>();
-    
-    for (int i =0; i <= 6; i++)  {
-    int number = toString(rand.nextInt(50));
-      coupon.add(number);
-    }
 
-  System.out.println(coupon);
-  }
+public class Lotto {
+    public static void main(String[] args) {
+        HashSet<Integer> numbers = new HashSet<>();
+        Random rand  = new Random();
+        while (numbers.size() <  6) {
+            numbers.add(rand.nextInt(1, 50));
+        }
+        System.out.println(numbers);
+    }
 }
