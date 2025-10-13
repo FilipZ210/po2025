e811fcb87154a667b9aa5aee4cbc6860e99c16db
Filip <zajac@filip.com>
2025-10-13 18:01:29 +0200

    Losowanie i Symulacja Lotto

---
+++ Lab2/Lotto.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+
+public class Lotto {
+    public static void main(String[] args) {
+        ArrayList<Integer> my_numbers = new ArrayList<>();
+        for(int i = 0; i < args.length; i++){
+            my_numbers.add(Integer.parseInt(args[i]));
+        }
+
+        for(int i = 0; i < args.length; i++){
+            if (Integer.parseInt(args[i]) > 49 || Integer.parseInt(args[i]) < 0) {
+                System.out.println(args[i] + " jest zlym numerem, dopuszczone sa liczzby od 1 do 49");
+                System.exit(0);
+            }
+        }
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random rand  = new Random();
+        while (numbers.size() <  6) {
+            int new_number = rand.nextInt(1, 50);
+            if(numbers.contains(new_number)) {
+                //do nothing
+            } else {
+                numbers.add(new_number);
+            }
+
+
+        }
+        int guesses = 0;
+
+        for( int n : my_numbers){
+            if(numbers.contains(n)) {
+                guesses += 1;
+            }
+        }
+
+        System.out.println("Twoje typy: " + my_numbers);
+        System.out.println("Wylosowane liczby " + numbers);
+        System.out.println("Liczba trafien "+ guesses);
+    }
+}
---
+++ Lab2/Symulacja.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+
+public class Symulacja {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random rand  = new Random();
+        while (numbers.size() <  6) {
+            int new_number = rand.nextInt(1, 50);
+            if(numbers.contains(new_number)) {
+                //do nothing
+            } else {
+                numbers.add(new_number);
+            }
+        }
+
+        int matches = 0;
+        int ilosc_prob = 0;
+        while (matches < 6) {
+            ilosc_prob++;
+            matches = 0;
+            ArrayList<Integer> trafione = new ArrayList<>();
+            while(trafione.size() < 6) {
+                int new_number = rand.nextInt(1, 50);
+                if (trafione.contains(new_number)) {
+
+                } else {
+                    trafione.add(new_number);
+                }
+            }
+
+            for (int n : trafione) {
+                if(numbers.contains(n)) {
+                    matches++;
+                }
+            }
+        }
+        System.out.println(numbers);
+        System.out.println("Liczba prob wynosi " + ilosc_prob);
+    }
+}
